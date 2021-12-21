3c75a8e97e20860c2d8c6c510bb928c0266c4cc9
wabliplatform <93119230+wabliplatform@users.noreply.github.com>
2021-12-21 10:39:00 +0200

    My commit message

--- javascript/Home.js
+++ javascript/Home.js
@@ -1,4 +1,8 @@
-let apiBlogApi = new TempApi.BlogApi();import TempApi from '../src/index';document.getElementById('ilrqo').onclick = (event) => {
+let apiBlogApi = new TempApi.BlogApi();import TempApi from '../src/index';document.getElementById('isif').onclick = (event) => {
+    event.preventDefault();
+    { location.href= '/Home';}};document.getElementById('id022').onclick = (event) => {
+    event.preventDefault();
+    { location.href= '/Home';}};document.getElementById('ilrqo').onclick = (event) => {
     event.preventDefault();
     { location.href= '/createpost';}};window.onload = () => {apiBlogApi.getAllblog((error, data, response) => { if (error) {console.error(error);} else { console.log('API called successfully. Returned data: ' + data); const subDataElements = document.getElementById("i1pxk").querySelectorAll( "[dataitem='true']" );
   data.forEach((item,i) => {
@@ -24,7 +28,24 @@ let apiBlogApi = new TempApi.BlogApi();import TempApi from '../src/index';docume
         if(insideSubDataElement !== null){
           insideSubDataElement.textContent = data[data.length -i -1].posttext;
         }
+       } catch (e) { console.log(e) };try { 
+        const insideSubDataElement = subDataElements[i].querySelector("[annotationname = 'viewpost']");
+        if(insideSubDataElement !== null){
+          insideSubDataElement.undefined = data[data.length -i -1].viewpost;
+        }
        } catch (e) { console.log(e) };
       }
     });
-    [...subDataElements].forEach((element,index) => {if(index >= data.length) subDataElements[index].remove()})}});};
+    [...subDataElements].forEach((element,index) => {if(index >= data.length) subDataElements[index].remove()})}});};document.getElementById('isa8p').onclick = (event) => {
+    event.preventDefault();
+    { location.href= '/viewpost';}};document.getElementById('itqme').onclick = (event) => {
+    event.preventDefault();
+    { location.href= '/viewpost';}};window.onload = () => {let blogId = window.location.pathname.replace('/Home/','');apiBlogApi.getblog( blogId, (error, data, response) => { if (error) {console.error(error);} else { console.log('API called successfully. Returned data: ' + data); document.querySelector('[annotationname = date]').textContent = response.body.query.date ;document.querySelector('[annotationname = title]').textContent = response.body.query.title ;document.querySelector('[annotationname = posttext]').textContent = response.body.query.posttext ;document.querySelector('[annotationname = image]').src = response.body.query.image ;}});};document.getElementById('ii6ud').onclick = (event) => {
+    event.preventDefault();
+    { location.href= '/viewpost';}};window.onload = () => {let blogId = window.location.pathname.replace('/Home/','');apiBlogApi.getblog( blogId, (error, data, response) => { if (error) {console.error(error);} else { console.log('API called successfully. Returned data: ' + data); document.querySelector('[annotationname = date]').textContent = response.body.query.date ;document.querySelector('[annotationname = title]').textContent = response.body.query.title ;document.querySelector('[annotationname = posttext]').textContent = response.body.query.posttext ;document.querySelector('[annotationname = image]').src = response.body.query.image ;}});};document.getElementById('iq8hq').onclick = (event) => {
+    event.preventDefault();
+    { location.href= '/viewpost';}};window.onload = () => {let blogId = window.location.pathname.replace('/Home/','');apiBlogApi.getblog( blogId, (error, data, response) => { if (error) {console.error(error);} else { console.log('API called successfully. Returned data: ' + data); document.querySelector('[annotationname = date]').textContent = response.body.query.date ;document.querySelector('[annotationname = title]').textContent = response.body.query.title ;document.querySelector('[annotationname = posttext]').textContent = response.body.query.posttext ;document.querySelector('[annotationname = image]').src = response.body.query.image ;}});};document.getElementById('i9aug').onclick = (event) => {
+    event.preventDefault();
+    { location.href= '/viewpost';}};window.onload = () => {let blogId = window.location.pathname.replace('/Home/','');apiBlogApi.getblog( blogId, (error, data, response) => { if (error) {console.error(error);} else { console.log('API called successfully. Returned data: ' + data); document.querySelector('[annotationname = date]').textContent = response.body.query.date ;document.querySelector('[annotationname = title]').textContent = response.body.query.title ;document.querySelector('[annotationname = posttext]').textContent = response.body.query.posttext ;document.querySelector('[annotationname = image]').src = response.body.query.image ;}});};document.getElementById('i2bd4').onclick = (event) => {
+    event.preventDefault();
+    { location.href= '/viewpost';}};window.onload = () => {let blogId = window.location.pathname.replace('/Home/','');apiBlogApi.getblog( blogId, (error, data, response) => { if (error) {console.error(error);} else { console.log('API called successfully. Returned data: ' + data); document.querySelector('[annotationname = date]').textContent = response.body.query.date ;document.querySelector('[annotationname = title]').textContent = response.body.query.title ;document.querySelector('[annotationname = posttext]').textContent = response.body.query.posttext ;document.querySelector('[annotationname = image]').src = response.body.query.image ;}});};
--- javascript/createpost.js
+++ javascript/createpost.js
@@ -1,3 +1,7 @@
-let apiBlogApi = new TempApi.BlogApi();import TempApi from '../src/index';let blog = new TempApi.Blog();document.getElementById('izz9c').onclick = (event) => {
+let apiBlogApi = new TempApi.BlogApi();import TempApi from '../src/index';let blog = new TempApi.Blog();document.getElementById('i4un6').onclick = (event) => {
+    event.preventDefault();
+    { location.href= '/Home';}};document.getElementById('iyypu').onclick = (event) => {
+    event.preventDefault();
+    { location.href= '/Home';}};document.getElementById('izz9c').onclick = (event) => {
     event.preventDefault();
     blog['date'] = document.querySelector("[annotationname = 'date']").value;blog['title'] = document.querySelector("[annotationname = 'title']").value;blog['posttext'] = document.querySelector("[annotationname = 'posttext']").value;blog['image'] = document.querySelector("[annotationname = 'image']").value;apiBlogApi.createblog( blog, (error, data, response) => { if (error) {console.error(error);} else { console.log('API called successfully. Returned data: ' + data); { location.href= '/Home/'+response.body.query._id+'';}}});};
